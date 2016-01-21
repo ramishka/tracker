e849223fb98c22107660ed7180dd175bb1afc2a3
ramishka <ramishka@gmail.com>
2016-01-21 13:59:12 +0530

    Added support for watch list.

--- src/scripts/ui/subscribedlist.ts
+++ src/scripts/ui/subscribedlist.ts
@@ -1,3 +1,28 @@
+import {Component, View} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Storage} from '../providers/providers';
+import {SeriesList} from './uicomponents';
+import {Show} from '../data/show';
+
+@Component({
+  selector: 'subscribed-shows' 
+})
+
+@View ({
+    templateUrl : 'subscribedlist.html',   
+    directives: [SeriesList, ROUTER_DIRECTIVES]
+})
+
 export class SubscribedList {
+
+  public subscribedShows: Show[];
+
+  constructor(private localStorage: Storage) {
+    this.subscribedShows = localStorage.getItem('subscribedShows', []);
+  }
+
+  unsubscribe(show: Show): void {
+    this.subscribedShows = this.subscribedShows.filter((subscribedShow: Show) => subscribedShow.id !== show.id);
+  }
 
 }
